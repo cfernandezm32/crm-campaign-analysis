5bde0684f1cffb97438efcb2ee8051bd112f1a72
cfernandezm32 <cfernandezm.05@gmail.com>
2026-07-06 15:44:39 -0500

    Update queries.sql

--- data/sql./queries.sql
+++ data/sql./queries.sql
@@ -1,1 +1,10 @@
+-- ===================================
+-- CRM Campaign Analysis
+-- Author: Camila Fernández
+-- ===================================
+
+-- 1. Total users
+
+SELECT COUNT(*) AS total_users
+FROM users;
 
